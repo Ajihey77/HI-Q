3812df990c3e074c39ca018555c443252e4e7d75
Ajihey77 <anjihey77@gmail.com>
2025-03-31 23:57:10 +0900

    🔥

--- src/containers/rooms/RoomsContainer.tsx
+++ src/containers/rooms/RoomsContainer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams, useParams } from "next/navigation";
 
 import RoomsMain from "./roomsMain";
